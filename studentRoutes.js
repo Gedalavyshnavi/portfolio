edaccb7671185e5565302b9d22dee40dbd7df06d
Gedalavyshnavi <143178487+Gedalavyshnavi@users.noreply.github.com>
2025-03-08 19:15:00 +0400

    Delete studentRoutes.js

--- studentRoutes.js
+++
@@ -1,7 +0,0 @@
-const studentcon=require('../control/studentControl')
-const express=require('express')
-const routes=express.Router()
-routes.post('/add-student',studentcon.insertStudent)
-routes.get('/get-student',studentcon.insertStudent)
-routes.put('/updateStudent/:id',studentcon.UpdateStudent)
-module.exports=routes
