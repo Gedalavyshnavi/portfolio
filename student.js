242a2119d71b8990bda92733292ee73a5e12aedb
Gedalavyshnavi <143178487+Gedalavyshnavi@users.noreply.github.com>
2025-03-08 19:14:26 +0400

    Delete student.js

--- student.js
+++
@@ -1,8 +0,0 @@
-const mongoose=require('mongoose')
-const studentSchema=new mongoose.Schema({
-    name:{type:String,requires:true},
-    age:{type:Number,requires:true},
-    class:{type:String,requires:true},
-    branch:{type:String,requires:true}
-})
-module.exports=mongoose.model('student',studentSchema)
