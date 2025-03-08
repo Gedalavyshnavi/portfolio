82293ea9587b841bcb8da04fde44adf2adc94968
Gedalavyshnavi <143178487+Gedalavyshnavi@users.noreply.github.com>
2025-03-08 19:07:26 +0400

    Delete app.js

--- app.js
+++
@@ -1,7 +0,0 @@
-let express= require("express");
-let app = express();
-app.use(express.json());
-let studentTour = require("./CONTROLLERS/studentTour");
-app.get("/student top tours",studentTour.getTopTours);
-app.get("/student tours",studentTour.getTours);
-module.exports={app};
